0ebbe5438110486c6fd1876412baeee447e515a3
Yuyi Wang <Strawberry_Str@hotmail.com>
2022-12-11 15:54:44 +0800

    Remove tempfile & copy affairs.

--- src/lib.rs
+++ src/lib.rs
@@ -97,59 +97,64 @@ impl From<Error> for io::Error {
     }
 }
 
-// File: 内建文件类型
 // size: 文件大小
-// offset: 文件偏移（目前指针位置），无需序列化
 // start_at: 文件在 source 中的开始位置
-// mime: 文件类型的 MIMETYPE
+#[derive(Serialize, Deserialize, Debug)]
+struct FileHeader {
+    size: u64,
+    start_at: u64,
+}
+
+impl FileHeader {
+    pub fn from_path(p: impl AsRef<Path>) -> Result<Self> {
+        Ok(Self {
+            size: std::fs::metadata(p)?.len(),
+            start_at: 0,
+        })
+    }
+}
+
+// File: 内建文件类型
+// offset: 文件偏移（目前指针位置），无需序列化
 // source: 文件源，无需序列化
 /// The file type returned by the open function implements most of the APIs of std::fs::File
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug)]
 pub struct File {
-    size: u64,
-    #[serde(skip)]
+    header: FileHeader,
     offset: u64,
-    start_at: u64,
-    mime: String,
-    #[serde(skip)]
-    source: Option<fs::File>,
+    source: fs::File,
 }
 
 impl File {
-    pub fn open<P: AsRef<path::Path>>(fs: &FRFS, path: P) -> Result<File> {
+    pub fn open(fs: &FRFS, path: impl AsRef<Path>) -> Result<File> {
         fs.open(path)
     }
 
     pub fn metadata(&self) -> Result<Metadata> {
-        Ok(Metadata(self.size, FileType(true)))
-    }
-
-    pub fn mimetype(&self) -> String {
-        self.mime.clone()
+        Ok(Metadata(self.header.size, FileType(true)))
     }
 }
 
 impl From<fs::File> for File {
     fn from(f: fs::File) -> Self {
         Self {
-            size: f.metadata().unwrap().len(),
+            header: FileHeader {
+                size: f.metadata().unwrap().len(),
+                start_at: 0,
+            },
             offset: 0,
-            start_at: 0,
-            mime: "".to_string(),
-            source: Some(f),
+            source: f,
         }
     }
 }
 
 impl Read for File {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
-        let mut file = match &self.source {
-            Some(f) => f,
-            None => return Err(Error::NotFound.into()),
-        };
-        let ptr_now = file.seek(SeekFrom::Start(self.start_at + self.offset))?;
-        let ptr_end = self.start_at + self.size;
-        let ret = file.take(ptr_end - ptr_now).read(buf)?;
+        let ptr_now = self
+            .source
+            .seek(SeekFrom::Start(self.header.start_at + self.offset))?;
+        let ptr_end = self.header.start_at + self.header.size;
+        let ret = self.source.try_clone()?.take(ptr_end - ptr_now).read(buf)?;
         self.offset += ret as u64;
         Ok(ret)
     }
@@ -159,12 +164,12 @@ impl Seek for File {
     fn seek(&mut self, pos: SeekFrom) -> Result<u64> {
         match pos {
             SeekFrom::Current(offset) => self.seek(SeekFrom::Current(offset)),
-            SeekFrom::Start(offset) => self.seek(SeekFrom::Start(self.start_at + offset)),
+            SeekFrom::Start(offset) => self.seek(SeekFrom::Start(self.header.start_at + offset)),
             SeekFrom::End(offset) => {
                 let offset = if offset > 0 {
-                    self.start_at + self.size
+                    self.header.start_at + self.header.size
                 } else {
-                    self.start_at + self.size - offset.unsigned_abs()
+                    self.header.start_at + self.header.size - offset.unsigned_abs()
                 };
                 self.seek(SeekFrom::Start(offset))
             }
@@ -300,18 +305,14 @@ impl DirEntry {
 // dirs: 此目录下文件
 #[derive(Serialize, Deserialize, Debug)]
 struct Dir {
-    files: HashMap<String, File>,
+    files: HashMap<String, FileHeader>,
     dirs: HashMap<String, Dir>,
 }
 
 impl Dir {
     // 使用本地文件系统填充 Dir 结构体
-    fn fill_with<P: AsRef<path::Path>>(
-        &mut self,
-        root: P,
-        path: P,
-        length: &mut u64,
-    ) -> Result<()> {
+    fn fill_with(&mut self, root: impl AsRef<Path>, path: impl AsRef<Path>) -> Result<u64> {
+        let mut length = 0;
         // 遍历目录
         for entry in fs::read_dir(path)? {
             let path = entry?.path();
@@ -325,18 +326,9 @@ impl Dir {
             };
             // 优先填充文件
             if path.is_file() {
-                let source = fs::File::open(&path)?;
-                let file = File {
-                    size: source.metadata()?.len(),
-                    offset: 0,
-                    start_at: 0,
-                    mime: mime_guess::from_path(&path)
-                        .first_or_octet_stream()
-                        .to_string(),
-                    source: Some(fs::File::open(path.clone())?),
-                };
-                *length += file.size;
-                self.files.insert(name, file);
+                let header = FileHeader::from_path(path)?;
+                length += header.size;
+                self.files.insert(name, header);
             } else if path.is_dir() {
                 // 构造子目录
                 let mut dir = Dir {
@@ -344,134 +336,47 @@ impl Dir {
                     dirs: HashMap::new(),
                 };
                 // 填充子目录
-                dir.fill_with(root.as_ref(), &path, length)?;
+                length += dir.fill_with(root.as_ref(), &path)?;
                 self.dirs.insert(name, dir);
             }
         }
-        Ok(())
+        Ok(length)
     }
 }
 
-// FRFS: 只读文件系统类型
+/// FRFS header.
 // root: 根目录'/', 用于存储文件所在位置
 // data_size: 在 base 文件中的 start_at 后所跟的数据大小
 // start_at: 在 base 文件中的开始点，无需序列化
+#[derive(Debug, Serialize, Deserialize)]
+struct Header {
+    pub root: Dir,
+    pub data_size: u64,
+    pub start_at: u64,
+}
+
+// FRFS: 只读文件系统类型
 // base: 文件源，无需序列化
 /// Read-only File System
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug)]
 pub struct FRFS {
-    root: Dir,
-    data_size: u64,
-    start_at: u64,
-    #[serde(skip)]
-    base: Option<fs::File>,
+    header: Header,
+    base: fs::File,
 }
 
 impl FRFS {
-    /// Create FRFS data from filesystem.
-    pub fn build_from_dir<P: AsRef<path::Path>>(source: P) -> Result<Vec<u8>> {
-        let embed_fs = Self::from_dir(source)?;
-        embed_fs.build()
-    }
-
-    // 从文件夹构造 FRFS 结构体，要求路径存在且为文件夹，有权限读取
-    fn from_dir<P: AsRef<path::Path>>(source: P) -> Result<Self> {
-        let source = source.as_ref();
-        let mut length: u64 = 0;
-        let mut dir = Dir {
-            files: HashMap::new(),
-            dirs: HashMap::new(),
-        };
-        dir.fill_with(source, source, &mut length)?;
-        Ok(Self {
-            root: dir,
-            data_size: length, /* 此length仅用作参考 */
-            start_at: 0,
-            base: None,
-        })
-    }
-
-    // 从 Dir 中读取文件路径`source`，不编码直接二进制写入 target，写入后增加 data_size(已写入的文件大小)
-    fn fill_with_files(dir: &mut Dir, target: &mut Vec<u8>, data_size: &mut u64) -> Result<()> {
-        for file in dir.files.values_mut() {
-            let mut source = file.source.as_ref().ok_or(Error::NotFound)?;
-            // 更新文件长度（调用完`from_dir`后，`build`前修改本地磁盘上的数据，文件长度可能变更）
-            file.size = source.metadata()?.len();
-            // 写入文件
-            let mut buf = Vec::with_capacity(file.size as usize);
-            source.read_to_end(&mut buf)?;
-            target.extend(buf);
-            // 确定文件偏移
-            file.start_at = *data_size;
-            *data_size += file.size;
-        }
-        for sub_dir in dir.dirs.values_mut() {
-            Self::fill_with_files(sub_dir, target, data_size)?;
-        }
-        Ok(())
-    }
-
-    // 将 FRFS 构建为 Vec<u8>
-    fn build(mut self) -> Result<Vec<u8>> {
-        // 构造数据体，需要比文件头先构建（构建数据时才知道数据头中的data_size），`+1024`防止溢出,
-        // 这里的`data_size`仅用于参考创建 Vec 时的长度，实际数据大小为下面的填充后的`data_size`
-        // 如果在调用完`from_dir`后，`build`前修改本地磁盘上的数据，则可能出现二者不一致的情况
-        let mut data: Vec<u8> = Vec::with_capacity(self.data_size as usize + 1024);
-        let mut data_size: u64 = 0;
-        Self::fill_with_files(&mut self.root, &mut data, &mut data_size)?;
-        // 此时`data_size`才是实际填充的数据大小
-        self.data_size = data_size;
-        // 限制文件头最大 100MiB
-        let serialize_options = bincode::DefaultOptions::new()
-            .with_fixint_encoding()
-            .allow_trailing_bytes()
-            .with_limit(104857600 /* 100MiB */);
-        // 构建文件头
-        let header = match serialize_options.serialize(&self) {
-            Ok(vec) => vec,
-            Err(e) => {
-                return Err(Error::SerializationError(e.to_string()).into());
-            }
-        };
-
-        // 构建 EmbedFS
-        let mut target: Vec<u8> = Vec::new();
-        // 写入 Magic Number
-        target.extend(MAGIC_NUMBER_START);
-        // 写入文件头长度
-        target.extend(header.len().to_be_bytes());
-        // 写入 bincode 编码的文件头
-        target.extend(&header);
-        // 写入文件数据
-        target.extend(&data);
-        // 计算此时 target 长度并写入
-        let target_length = target.len();
-        // U64_LEN 是 target_length 的长度
-        let target_length = target_length + USIZE_LEN;
-        let target_length = target_length + MAGIC_NUMBER_END.len();
-        target.extend(target_length.to_be_bytes());
-        // 写入文件尾
-        target.extend(MAGIC_NUMBER_END);
-
-        Ok(target)
-    }
-
-    /// Create frfs file from file system.
-    pub fn pack<P: AsRef<path::Path>>(source: P, target: P) -> Result<()> {
-        fs::write(target, Self::build_from_dir(source)?)?;
-        Ok(())
+    pub fn new(p: impl AsRef<Path>) -> Result<Self> {
+        Self::from_std_file(fs::File::open(p)?)
     }
 
     /// Read FRFS from File.
-    pub fn from_embed_file(mut file: File) -> Result<Self> {
-        let mut base = tempfile::NamedTempFile::new()?;
-        io::copy(&mut file, &mut base)?;
-        Self::new(base.path())
+    pub fn from_file(file: File) -> Result<Self> {
+        Self::from_std_file(file.source)
     }
 
     /// Load a FRFS file.
-    pub fn new<P: AsRef<path::Path> + Copy>(path: P) -> Result<Self> {
-        let mut base = fs::File::open(path)?;
+    pub fn from_std_file(f: fs::File) -> Result<Self> {
+        let mut base = f.try_clone()?;
         let base_length = base.metadata()?.len();
         let mut magic_number_start_data = [0; MAGIC_NUMBER_START.len()];
         let mut header_length_data = [0; USIZE_LEN];
@@ -499,7 +404,7 @@ impl FRFS {
         }
         base.read_exact(&mut header_length_data)?;
         // start_at 指 Data 段在 base 的位置
-        let start_at = base.seek(SeekFrom::Current(0))?;
+        let start_at = base.stream_position()?;
         let start_at = start_at
             + base
                 .take(u64::from_be_bytes(header_length_data))
@@ -509,16 +414,15 @@ impl FRFS {
             .with_fixint_encoding()
             .allow_trailing_bytes()
             .with_limit(104857600 /* 100MiB */);
-        let mut frofs: Self = match serialize_options.deserialize(&header_data) {
+        let mut header: Header = match serialize_options.deserialize(&header_data) {
             Ok(header) => header,
             Err(e) => {
                 return Err(Error::DeserializationError(e.to_string()).into());
             }
         };
-        frofs.base = Some(fs::File::open(path)?);
-        frofs.start_at = start_at;
+        header.start_at = start_at;
 
-        Ok(frofs)
+        Ok(Self { header, base: f })
     }
 
     // 在 FRFS 中递归打开路径里的文件的实现
@@ -532,14 +436,15 @@ impl FRFS {
                 .files
                 .get(&next_path)
                 .ok_or_else(|| Error::Unknown("contains key but no content".to_string()))?;
-            let source = self.base.as_ref().ok_or(Error::NotFound)?.try_clone()?;
+            let source = self.base.try_clone()?;
             // self.start_at + file.start_at 是这个 file 在 base 里的开始点
             Ok(File {
-                size: file.size,
+                header: FileHeader {
+                    size: file.size,
+                    start_at: self.header.start_at + file.start_at,
+                },
                 offset: 0, // 让File的文件指针指向0
-                start_at: self.start_at + file.start_at,
-                mime: file.mime.clone(),
-                source: Some(source),
+                source,
             })
         } else if current_dir.dirs.contains_key(&next_path) {
             let dir = current_dir
@@ -573,7 +478,7 @@ impl FRFS {
 
     /// Open the file, unix like path, with or without '/'
     /// at the beginning means starting from the root directory.
-    pub fn open<P: AsRef<path::Path>>(&self, path: P) -> Result<File> {
+    pub fn open(&self, path: impl AsRef<Path>) -> Result<File> {
         let path = normalize_path(path.as_ref());
         let path = if path.starts_with("/") {
             path.strip_prefix("/")
@@ -582,17 +487,17 @@ impl FRFS {
         } else {
             path
         };
-        self.open_file(&self.root, path.iter())
+        self.open_file(&self.header.root, path.iter())
     }
 
     /// Returns the file (or folder) information of the corresponding path.
-    pub fn metadata<P: AsRef<path::Path>>(&self, path: P) -> Result<Metadata> {
+    pub fn metadata(&self, path: impl AsRef<Path>) -> Result<Metadata> {
         let file = self.open(path)?;
         file.metadata()
     }
 
     /// Returns an iterator over the entries within a directory.
-    pub fn read_dir<P: AsRef<path::Path>>(&self, path: P) -> Result<ReadDir> {
+    pub fn read_dir(&self, path: impl AsRef<Path>) -> Result<ReadDir> {
         let path = normalize_path(path.as_ref());
         let path = if path.starts_with("/") {
             path.strip_prefix("/")
@@ -601,11 +506,11 @@ impl FRFS {
         } else {
             path
         };
-        let dir = Self::open_dir(&self.root, path.iter())?;
+        let dir = Self::open_dir(&self.header.root, path.iter())?;
         let mut dir_entrys: Vec<Result<DirEntry>> = dir
             .dirs
-            .iter()
-            .map(|(dir_name, _dir)| {
+            .keys()
+            .map(|dir_name| {
                 let mut path = path.clone();
                 path.push(dir_name);
                 Ok(DirEntry {
@@ -634,18 +539,114 @@ impl FRFS {
 }
 
 /// Load a FRFS file.
-pub fn load<P: AsRef<path::Path> + Copy>(path: P) -> Result<FRFS> {
+pub fn load(path: impl AsRef<Path>) -> Result<FRFS> {
     FRFS::new(path)
 }
 
 /// Load a FRFS file from frfs::File.
-pub fn load_from_embed_file(file: File) -> Result<FRFS> {
-    FRFS::from_embed_file(file)
+pub fn load_from_file(file: File) -> Result<FRFS> {
+    FRFS::from_file(file)
+}
+
+#[derive(Debug)]
+pub struct FRFSBuilder {
+    header: Header,
+    source: PathBuf,
+}
+
+impl FRFSBuilder {
+    // 从文件夹构造 FRFS 结构体，要求路径存在且为文件夹，有权限读取
+    pub fn from_dir(source: impl AsRef<Path>) -> Result<Self> {
+        let source = source.as_ref();
+        let mut dir = Dir {
+            files: HashMap::new(),
+            dirs: HashMap::new(),
+        };
+        let length = dir.fill_with(source, source)?;
+        Ok(Self {
+            header: Header {
+                root: dir,
+                data_size: length,
+                start_at: 0,
+            },
+            source: source.to_path_buf(),
+        })
+    }
+
+    // 从 Dir 中读取文件路径`source`，不编码直接二进制写入 target，写入后增加 data_size(已写入的文件大小)
+    fn fill_with_files(
+        p: &Path,
+        dir: &mut Dir,
+        target: &mut Vec<u8>,
+        mut data_size: u64,
+    ) -> Result<u64> {
+        for (name, file) in &mut dir.files {
+            // 写入文件
+            let mut buf = Vec::with_capacity(file.size as usize);
+            let mut source = fs::File::open(p.join(name))?;
+            source.read_to_end(&mut buf)?;
+            let len = buf.len();
+            target.extend(buf);
+            // 确定文件偏移
+            file.start_at = data_size;
+            file.size = len as u64;
+            data_size += file.size;
+        }
+        for (name, sub_dir) in &mut dir.dirs {
+            data_size += Self::fill_with_files(&p.join(name), sub_dir, target, data_size)?;
+        }
+        Ok(data_size)
+    }
+
+    // 将 FRFS 构建为 Vec<u8>
+    fn build(mut self) -> Result<Vec<u8>> {
+        // 构造数据体，需要比文件头先构建（构建数据时才知道数据头中的data_size），`+1024`防止溢出,
+        // 这里的`data_size`仅用于参考创建 Vec 时的长度，实际数据大小为下面的填充后的`data_size`
+        // 如果在调用完`from_dir`后，`build`前修改本地磁盘上的数据，则可能出现二者不一致的情况
+        let mut data: Vec<u8> = Vec::with_capacity(self.header.data_size as usize + 1024);
+        let data_size = Self::fill_with_files(&self.source, &mut self.header.root, &mut data, 0)?;
+        // 此时`data_size`才是实际填充的数据大小
+        self.header.data_size = data_size;
+        // 限制文件头最大 100MiB
+        let serialize_options = bincode::DefaultOptions::new()
+            .with_fixint_encoding()
+            .allow_trailing_bytes()
+            .with_limit(104857600 /* 100MiB */);
+        // 构建文件头
+        let header = match serialize_options.serialize(&self.header) {
+            Ok(vec) => vec,
+            Err(e) => {
+                return Err(Error::SerializationError(e.to_string()).into());
+            }
+        };
+
+        // 构建 EmbedFS
+        let mut target: Vec<u8> = Vec::new();
+        // 写入 Magic Number
+        target.extend(MAGIC_NUMBER_START);
+        // 写入文件头长度
+        target.extend(header.len().to_be_bytes());
+        // 写入 bincode 编码的文件头
+        target.extend(&header);
+        // 写入文件数据
+        target.extend(&data);
+        // 计算此时 target 长度并写入
+        let target_length = target.len();
+        // U64_LEN 是 target_length 的长度
+        let target_length = target_length + USIZE_LEN;
+        let target_length = target_length + MAGIC_NUMBER_END.len();
+        target.extend(target_length.to_be_bytes());
+        // 写入文件尾
+        target.extend(MAGIC_NUMBER_END);
+
+        Ok(target)
+    }
 }
 
 /// Pack a folder into a FRFS file.
-pub fn pack<P: AsRef<path::Path>>(source: P, target: P) -> Result<()> {
-    FRFS::pack(source, target)
+pub fn pack(source: impl AsRef<Path>, target: impl AsRef<Path>) -> Result<()> {
+    fs::write(target, FRFSBuilder::from_dir(source)?.build()?)?;
+    Ok(())
 }
 
 // Normalize all intermediate components of the path
