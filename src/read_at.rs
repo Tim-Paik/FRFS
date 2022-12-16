b79cc81bec05452594c7a87faa1d938ca267a715
Yuyi Wang <Strawberry_Str@hotmail.com>
2022-12-16 09:04:23 +0800

    Add comments for seek_read.

--- src/read_at.rs
+++ src/read_at.rs
@@ -3,6 +3,14 @@ use std::{fs::File, io::Result};
 #[cfg(windows)]
 pub fn read_at(f: &File, buf: &mut [u8], offset: u64) -> Result<usize> {
     use std::os::windows::fs::FileExt;
+    // Note we use `seek_read` here to imitate `read_at`.
+    //
+    // `read_at` is `pread`/`pread64` on Unix, which will not update file
+    // cursor after reading. However on Windows, all reading operations
+    // is finally `NtReadFile`, which will certainly update the file cursor.
+    //
+    // We use `seek_read` here because we don't care about the file cursor
+    // at all. We just need an atomic operation to seek and read.
     f.seek_read(buf, offset)
 }
 
